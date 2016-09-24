6b66550602fd00648ee9808d23d398b2cd270a05
Jean Carlos P. Bairro <jeancasulo@gmail.com>
2016-09-23 22:07:02 -0300

    Adicionada função gaussiana e geradas 100 amostras a partir do padrão

--- app/js/test.js
+++ app/js/test.js
@@ -24,6 +24,7 @@ function TestTable(testRows) {
 function startTests(xml, searcher) {
 	var table1 = test(xml, searcher);
 	var table2 = test(xml, searcher);
+	var testsFromGaussianDist = testFromGaussianDist();
 
 
 	console.log("------------ tabela 1 ---------------------")
@@ -35,11 +36,30 @@ function startTests(xml, searcher) {
 	for (var i = 0; i < table2.rows.length; i++) {
 		console.log(i + ": " + table2.rows[i].time);
 	};
+
+	console.log("-----geradas a partir dist. gaussiana---------")
+	for (var i = 0; i < testsFromGaussianDist.length; i++) {
+		console.log(i + ": " + testsFromGaussianDist[i]);
+	};
+}
+
+function testFromGaussianDist() { 
+	var tests = [];
+
+	var standard = gaussian(1.11, 0.583);
+	
+	for(var i = 0; i < 100; i++) {
+    	tests.push(standard());
+	}
+
+	tests.sort();
+
+	return tests;
 }
 
 function test(xml, searcher) {
 	var tests = [];
-
+	
 	for (var i = 0; i < 100; i++) {
 		var graph = createGraphFromXml(xml); /* O grafo é criado novamente a cada iteração, por causa do cache */
 		graph.orderVertices();
@@ -57,4 +77,35 @@ function test(xml, searcher) {
 	table.sort();
 
 	return table;
+}
+
+/*http://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve*/
+function gaussian(mean, stdev) {
+    var y2;
+    var use_last = false;
+    return function() {
+        var y1;
+        if(use_last) {
+           y1 = y2;
+           use_last = false;
+        }
+        else {
+            var x1, x2, w;
+            do {
+                 x1 = 2.0 * Math.random() - 1.0;
+                 x2 = 2.0 * Math.random() - 1.0;
+                 w  = x1 * x1 + x2 * x2;               
+            } while( w >= 1.0);
+            w = Math.sqrt((-2.0 * Math.log(w))/w);
+            y1 = x1 * w;
+            y2 = x2 * w;
+            use_last = true;
+       }
+
+       var retval = mean + stdev * y1;
+       if(retval > 0) 
+           return retval;
+       
+       return -retval;
+   }
 }
